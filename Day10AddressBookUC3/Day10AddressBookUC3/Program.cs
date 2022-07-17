a86b119d7b2f4c9b732a166161bbb7ab60684be2
Hemantdhurve <hemantdhurve007@gmail.com>
2022-07-18 02:30:17 +0530

    Added Day10 AddressBook file

---
+++ Day10AddressBookUC3/Day10AddressBookUC3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Day10AddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book");
+            ///Creates a reference of AddressBook
+            AddressBook addressBook = new AddressBook();
+            addressBook.AddContact();
+        }
+    }
+}
